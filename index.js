30b4ec76bf54705074708d3a201a9b73eddd5565
Curran Kelleher <curran.kelleher@gmail.com>
2020-09-26 07:41:21 -0400

    Add tests for multiple exercises

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const contains = (line, symbol) => ~line.indexOf(symbol);
 
 export const parse = (rawText) => {
-  const lines = rawText.split("\n").filter((line) => line.trim() !== "");
+  const lines = rawText
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line !== "");
+
   let exercise;
+
   const rows = [];
 
   lines.forEach((line) => {
@@ -23,14 +28,15 @@ export const parse = (rawText) => {
 
       const row = { exercise, reps, poundage };
       if (numbersAfterAt[0].length < afterAt.length) {
-        row.notes = afterAt.substring(numbersAfterAt[0].length).trim();
+        row.notes = afterAt.substring(numbersAfterAt[0].length);
       }
       for (let i = 0; i < multiplier; i++) {
         rows.push(row);
       }
     } else {
-      exercise = line.trim();
+      exercise = line;
     }
   });
+
   return rows;
 };
--- test.js
+++ test.js
@@ -3,16 +3,26 @@ const { parse } = require(".");
 
 describe("parse", function () {
   const squatSet = { exercise: "Squat", reps: 5, poundage: 165 };
+    const twoSquatSets = [squatSet, squatSet];
+    const threeSquatSets = [squatSet, squatSet, squatSet];
+    const fourSquatSets = [squatSet, squatSet, squatSet, squatSet];
+
   it("should parse an exercise with sets and reps", () => {
     assert.deepEqual(parse("Squat\n5@165"), [squatSet]);
+    assert.deepEqual(parse("   Squat\n5@165   "), [squatSet]);
   });
-  describe("should parse an exercise with a multiplier", () => {
-    const threeSquatSets = [squatSet, squatSet, squatSet];
-    it("multiplier as 'x'", () => {
-      assert.deepEqual(parse("Squat\n3x5@165"), threeSquatSets);
-    });
-    it("multiplier as 'X'", () => {
-      assert.deepEqual(parse("Squat\n3x5@165"), threeSquatSets);
-    });
+
+  it("should parse multiple exercises", () => {
+    assert.deepEqual(parse("Squat\n5@165\nSquat\n5@165"), twoSquatSets);
+    assert.deepEqual(parse("Squat\n5@165\n\nSquat\n5@165"), twoSquatSets);
+  });
+
+  it("should parse an exercise with multiplier as 'x'", () => {
+    assert.deepEqual(parse("Squat\n2x5@165"), twoSquatSets);
+    assert.deepEqual(parse("Squat\n3x5@165"), threeSquatSets);
+  });
+
+  it("should parse an exercise with multiplier as 'X'", () => {
+    assert.deepEqual(parse("Squat\n4x5@165"), fourSquatSets);
   });
 });
